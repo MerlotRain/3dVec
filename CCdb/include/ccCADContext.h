7d8093446bc5b6a80c5316dd7422838047c144a2
MerlotRain <122578849+MerlotRain@users.noreply.github.com>
2024-09-05 10:31:18 +0800

    add glcad include files

---
+++ CCdb/include/ccCADContext.h
@@ -0,0 +1,12 @@
+#ifndef CCCADCONTEXT_H
+#define CCCADCONTEXT_H
+
+
+class ccCADContext : public ccHObject
+{
+public:
+    ccCADContext();
+    
+};
+
+#endif
---
+++ gl2d/ccCADShape.h
@@ -0,0 +1,9 @@
+#ifndef CCCADSHAPE_H
+#define CCCADSHAPE_H
+
+class ccCADShape
+{
+
+};
+
+#endif
